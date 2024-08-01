f86203c30d20c6061627cc98d93859bd7bc52a38
KENMEGNE YOH IVANA <125506456+yohivana-KYIM@users.noreply.github.com>
2024-08-01 21:01:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import incomeRoutes from "./routes/incomeRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
+import authenticateUser from "./middlewares/authenticateUser.js";
+
+// ⚠️⚠️⚠️ Note ⚠️⚠️⚠️
+// Si vous êtes un développeur consultant ce code dans mon dépôt, veuillez créer votre propre fichier .env avec les variables d'environnement nécessaires, car il n'est pas fourni dans ce dépôt.
+
+// Configuration des variables d'environnement
+dotenv.config();
+
+// Configuration de l'application
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Configuration de CORS
+app.use(
+  cors({
+    origin: "https://expense-davincit.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+// Routes
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/incomes", authenticateUser, incomeRoutes);
+app.use("/api/v1/expenses", authenticateUser, expenseRoutes);
+
+// Démarrer le serveur
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Serveur démarré sur le PORT ${PORT} !`);
+    });
+  } catch (error) {
+    console.error(`Erreur lors du démarrage du serveur : ${error.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
---
+++ utils/generateCookie.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const generateCookie = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "30d",
+  });
+
+  res.cookie("session", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "None",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+
+  return token;
+};
+
+export default generateCookie;
